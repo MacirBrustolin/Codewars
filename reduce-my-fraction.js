bf0092819b3fdba5760e7a0db1942ec1b03f7bb6
Macir Brustolin <mj.brustolin@hotmail.com>
2022-08-05 18:57:53 -0300

    Create reduce-my-fraction.js
    7 kyu

---
+++ reduce-my-fraction.js
@@ -0,0 +1,20 @@
+/*
+Write a function which reduces fractions to their simplest form! Fractions will be presented as an array/tuple (depending on the language) of strictly positive integers, and the reduced fraction must be returned as an array/tuple:
+
+input:   [numerator, denominator]
+output:  [reduced numerator, reduced denominator]
+example: [45, 120] --> [3, 8]
+All numerators and denominators will be positive integers.
+
+Note: This is an introductory Kata for a series... coming soon!
+*/
+
+function reduce(fraction) {
+  let num = fraction[0]
+  let den = fraction[1]
+  for (let i = num; i > 0; i--){
+    if (num % i ==0 && den % i ==0){
+      return [num/i, den/i]
+    }
+  }
+}
